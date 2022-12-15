47c9e8b8e392214bcdc4444de3e65d60f4aa62a9
Valeriia.Kurylko <reviliya@gmail.com>
2022-12-15 12:03:57 +0100

    working routes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import React from "react";
-import { Route, Routes, Link, Router } from "react-router-dom";
-import FoodInfoPage from "../pages/foodInfo";
-import Home from "../pages/home";
+import AppRoutes from "./components/router";
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/food-info" element={<FoodInfoPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+    <AppRoutes />
+    </div>
   );
 }
     
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AppRoutes from './components/router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AppRoutes>
     <App />
+    </AppRoutes>
   </React.StrictMode>
 );
 
