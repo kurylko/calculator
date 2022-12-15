6b71f98ea337166a6ba926c9be2044d1431681e5
Valeriia.Kurylko <reviliya@gmail.com>
2022-12-15 10:47:13 +0100

    button added

---
+++ src/pages/foodInfo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "@mui/material";
+
+export default function FoodInfoPage(){
+    return (
+        <div>
+            <Button variant="contained">Add food info</Button>
+        </div>
+    )
+}
