81802326f95f113c8a01a0ba63e50cc43e466011
kurylko <reviliya@gmail.com>
2023-02-21 21:36:18 +0100

    controlled inputs fot adding food info done

--- src/pages/foodInfo.jsx
+++ src/pages/foodInfo.jsx
@@ -1,13 +1,106 @@
 import React from "react";
-import { Button } from "@mui/material";
+import {Button} from "@mui/material";
+import {Input, TextField} from '@mui/material';
+import {useFormControl} from '@mui/material/FormControl';
+import {useState} from "react";
+import {Link} from "react-router-dom";
 
 
-export default function FoodInfoPage(){
+export default function FoodInfoPage() {
+
+    const [addFood, setAddFood] = useState({
+        foodName: "",
+        fat: "",
+        carbohydrate: "",
+        protein: "",
+        calories: "",
+        weight: "",
+    });
+
+    function handleChange(evt) {
+        const value = evt.target.value;
+        setAddFood({
+            ...addFood,
+            [evt.target.name]: value
+        });
+        console.log(evt.target.name, ":",  value)
+    };
+
+    function handleSubmit(evt) {
+        const value = evt.target.value;
+        setAddFood({
+            ...addFood,
+            [evt.target.name]: value
+        });
+    };
+
     return (
-        <div>
-            <h1>Food info page</h1>
-            <h2>Add food info</h2>
-            <Button variant="contained">Add food info</Button>
+        <div style={{display: "flex", justifyContent: "center"}}>
+            <div style={{display: "flex", flexDirection: "column", justifyContent: "center", width: "85%"}}>
+            <h2 style={{textAlign: "center", paddingBottom: "30px"}}>ADD FOOD INFO</h2>
+            <div style={{display: "flex", flexDirection: "row", flexWrap: "wrap", gap: "10px", paddingBottom: "30px"}}>
+                <TextField
+                    fullWidth
+                    required
+                    id="outlined-required"
+                    label="Food name"
+                    name="foodName"
+                    value={addFood.foodName}
+                    onChange={handleChange}
+                />
+                <TextField
+                    required
+                    id="outlined-number"
+                    label="Fat"
+                    type="number"
+                    name="fat"
+                    value={addFood.fat}
+                    onChange={handleChange}
+                />
+                <TextField
+                    required
+                    id="outlined-number"
+                    label="Protein"
+                    type="number"
+                    name="protein"
+                    value={addFood.protein}
+                    onChange={handleChange}
+                />
+                <TextField
+                    required
+                    id="outlined-number"
+                    label="Carbohydrate"
+                    type="number"
+                    name="carbohydrate"
+                    value={addFood.carbohydrate}
+                    onChange={handleChange}
+                />
+                <TextField
+                    required
+                    id="outlined-number"
+                    label="Calories, kcal"
+                    type="number"
+                    name="calories"
+                    value={addFood.calories}
+                    onChange={handleChange}
+                />
+                <TextField
+                    required
+                    id="outlined-number"
+                    label="Standard pack weight, g"
+                    type="number"
+                    name="weight"
+                    value={addFood.weight}
+                    onChange={handleChange}
+                />
+            </div>
+            <div style={{display: "flex", justifyContent: "flex-end", gap: "15px"}}>
+                <Button variant="contained" >Add food info</Button>
+                <Link to={"/"} style={{textDecoration: "none"}}>
+                <Button variant="contained">Cancel</Button>
+                    </Link>
+            </div>
+            </div>
         </div>
     )
 }
