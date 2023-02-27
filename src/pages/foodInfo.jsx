98afc676d554024029b2a0944ae98c8ea6d422ba
kurylko <reviliya@gmail.com>
2023-02-27 19:18:23 +0100

    test!

--- src/pages/foodInfo.jsx
+++ src/pages/foodInfo.jsx
@@ -27,7 +27,6 @@ export default function FoodInfoPage() {
         console.log("handler:", addFood);
     };
 
-
     return (
         <div style={{display: "flex", justifyContent: "center"}}>
             <div style={{display: "flex", flexDirection: "column", justifyContent: "center", width: "85%"}}>
