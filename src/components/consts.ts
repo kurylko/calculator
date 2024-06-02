d13b55c549684416a38e314cd5664a974e8cf3b8
kurylko <reviliya@gmail.com>
2024-06-02 16:21:05 +0200

    button added

--- src/components/consts.ts
+++ src/components/consts.ts
@@ -1,4 +1,4 @@
 
 export const FRONTEND_ROUTE_HOME = "/"
 export const FRONTEND_ROUTE_FOOD_INFO = "/food-info"
-export const HOME_TEXT = " hello home page"
+export const HOME_TEXT = "hello home page"
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import avocadoSmall from "../images/avocado-small.jpg";
 import {HOME_TEXT} from "../components/consts";
+import {Button} from "@mui/material";
 
 export default function Home() {
     return (
         <div className="home">
-            <h1 className="home-page">Save your favourite food here! </h1>
-            <div style={{display:"flex", gap: "20px"}}>
+            <h1>Save your favourite food here! </h1>
+            <div style={{display: "flex", gap: "20px", marginBottom: "30px"}}>
                 <img src={avocadoSmall} alt="Avocado" style={{width: '350px', height: '350px'}}/>
                 <div>{HOME_TEXT}</div>
             </div>
+            <Button
+                variant="outlined" size="medium" href="/food-info"
+                style={{width: '170px'}}
+                sx={{
+                    margin: 'auto',
+                    display: 'block'
+                }}>Start</Button>
         </div>
     )
 }
