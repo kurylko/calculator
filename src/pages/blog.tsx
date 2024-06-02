dc970b015d4aafea7fce226e57b50af386dac18b
kurylko <reviliya@gmail.com>
2024-06-02 15:08:57 +0200

    working menu fo3 3 pages

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Blog() {
     return (
         <div>
-            <h1 className="blog">BLOG </h1>
+            <h1 className="blog">BLOG is here </h1>
         </div>
     )
 }
