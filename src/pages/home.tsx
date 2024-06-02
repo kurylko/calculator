11273553177fd15bd9d21e05479ec9adb40800c1
kurylko <reviliya@gmail.com>
2024-06-02 16:04:47 +0200

    home page layout

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import avocadoSmall from "../images/avocado-small.jpg";
+import {HOME_TEXT} from "../components/consts";
 
 export default function Home() {
     return (
-        <div>
-            <h1 className="home-page">Home page </h1>
+        <div className="home">
+            <h1 className="home-page">Save your favourite food here! </h1>
+            <div style={{display:"flex", gap: "20px"}}>
+                <img src={avocadoSmall} alt="Avocado" style={{width: '350px', height: '350px'}}/>
+                <div>{HOME_TEXT}</div>
+            </div>
         </div>
     )
 }
