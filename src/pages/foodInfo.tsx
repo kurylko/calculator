96678e5f90b5f3af65da3a4499b54680f5e18e1f
kurylko <reviliya@gmail.com>
2024-09-09 14:04:43 +0200

    last added food for display

--- src/pages/foodInfo.tsx
+++ src/pages/foodInfo.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import {Button} from "@mui/material";
-import { TextField} from '@mui/material';
+import {TextField} from '@mui/material';
 import {useState, useEffect} from "react";
 import {Link} from "react-router-dom";
 import {IFoodItem} from "../interfaces/foodItem";
+import LastFoodAdded from "../components/LastFoodAdded";
 
 
 export default function FoodInfoPage() {
-    const { foodName = '', fat = '', protein = '', carbohydrate = '', calories = '', weight = ''  } = {}
+    const {foodName = '', fat = '', protein = '', carbohydrate = '', calories = '', weight = ''} = {}
 
     const lastInputFoodItemString = localStorage.getItem("lastInputFood");
 
@@ -16,14 +17,16 @@ export default function FoodInfoPage() {
         console.error('No data found in localStorage');
     }
 
-    const lastInputFoodItem : IFoodItem = lastInputFoodItemString ? JSON.parse(lastInputFoodItemString) : null;
+    const lastInputFoodItem: IFoodItem = lastInputFoodItemString ? JSON.parse(lastInputFoodItemString) : null;
 
-    const [addFood, setAddFood] = useState<IFoodItem>( !!lastInputFoodItem ? lastInputFoodItem :
-    {foodName, fat, protein, carbohydrate, calories, weight});
+    const [lastInputFoodItems, setLastInputFoodItems] = useState<IFoodItem[]>([]);
+
+    const [addFood, setAddFood] = useState<IFoodItem>(!!lastInputFoodItem ? lastInputFoodItem :
+        {foodName, fat, protein, carbohydrate, calories, weight});
 
     function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
         //const value = e.target.value;
-        const { target: {value}= {}} = e;
+        const {target: {value} = {}} = e;
         setAddFood({
             ...addFood,
             [e.target.name]: value
@@ -37,79 +40,109 @@ export default function FoodInfoPage() {
 
     const handleSubmit = () => {
         console.log("handler of Add food btn:", addFood);
+        setLastInputFoodItems((prevItems) => [...prevItems, addFood]);
+        console.log(lastInputFoodItems);
     };
 
+    useEffect(() => {
+        console.log("lastInputFoodItems updated:", lastInputFoodItems);
+    }, [lastInputFoodItems]);
+
+
     return (
-        <div style={{display: "flex", justifyContent: "center"}}>
-            <div style={{display: "flex", flexDirection: "column", justifyContent: "center", width: "85%"}}>
-                <h2 style={{textAlign: "center", paddingBottom: "30px"}}>ADD FOOD INFO</h2>
-                <div style={{
-                    display: "flex",
-                    flexWrap: "wrap",
-                    gap: "10px",
-                    paddingBottom: "30px"
-                }}>
-                    <TextField
-                        fullWidth
-                        required
-                        id="outlined-required"
-                        label="Food name"
-                        name="foodName"
-                        value={addFood.foodName}
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        required
-                        id="outlined-number"
-                        label="Fat"
-                        type="number"
-                        name="fat"
-                        value={addFood.fat}
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        required
-                        id="outlined-number"
-                        label="Protein"
-                        type="number"
-                        name="protein"
-                        value={addFood.protein}
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        required
-                        id="outlined-number"
-                        label="Carbohydrate"
-                        type="number"
-                        name="carbohydrate"
-                        value={addFood.carbohydrate}
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        required
-                        id="outlined-number"
-                        label="Calories, kcal"
-                        type="number"
-                        name="calories"
-                        value={addFood.calories}
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        required
-                        id="outlined-number"
-                        label="Standard pack weight, g"
-                        type="number"
-                        name="weight"
-                        value={addFood.weight}
-                        onChange={handleChange}
-                    />
+        <div style={{width: '100%', display: 'flex', flexDirection: 'column', gap: '50px'}}>
+            <div style={{display: "flex", justifyContent: "center"}}>
+                <div style={{display: "flex", flexDirection: "column", justifyContent: "center", width: "85%"}}>
+                    <h2 style={{textAlign: "center", paddingBottom: "30px"}}>ADD FOOD INFO</h2>
+                    <div style={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                        gap: "10px",
+                        paddingBottom: "30px"
+                    }}>
+                        <TextField
+                            fullWidth
+                            required
+                            id="outlined-required"
+                            label="Food name"
+                            name="foodName"
+                            value={addFood.foodName}
+                            onChange={handleChange}
+                        />
+                        <TextField
+                            required
+                            id="outlined-number"
+                            label="Fat"
+                            type="number"
+                            name="fat"
+                            value={addFood.fat}
+                            onChange={handleChange}
+                        />
+                        <TextField
+                            required
+                            id="outlined-number"
+                            label="Protein"
+                            type="number"
+                            name="protein"
+                            value={addFood.protein}
+                            onChange={handleChange}
+                        />
+                        <TextField
+                            required
+                            id="outlined-number"
+                            label="Carbohydrate"
+                            type="number"
+                            name="carbohydrate"
+                            value={addFood.carbohydrate}
+                            onChange={handleChange}
+                        />
+                        <TextField
+                            required
+                            id="outlined-number"
+                            label="Calories, kcal"
+                            type="number"
+                            name="calories"
+                            value={addFood.calories}
+                            onChange={handleChange}
+                        />
+                        <TextField
+                            required
+                            id="outlined-number"
+                            label="Standard pack weight, g"
+                            type="number"
+                            name="weight"
+                            value={addFood.weight}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div style={{display: "flex", justifyContent: "flex-end", gap: "16px"}}>
+                        <Button variant="contained" onClick={handleSubmit}>Add food info</Button>
+                        <Link to={"/"} style={{textDecoration: "none"}}>
+                            <Button variant="contained">Cancel</Button>
+                        </Link>
+                    </div>
                 </div>
-                <div style={{display: "flex", justifyContent: "flex-end", gap: "16px"}}>
-                    <Button variant="contained" onClick={handleSubmit}>Add food info</Button>
-                    <Link to={"/"} style={{textDecoration: "none"}}>
-                        <Button variant="contained">Cancel</Button>
-                    </Link>
-                </div>
+
+            </div>
+
+            <div style={{display: 'flex', flexDirection: 'column', gap: '30px', justifyContent: 'center', alignItems: 'center'}}>
+                <h2 style={{textAlign: "center", paddingBottom: "30px"}}>LAST FOOD YOU ADDED</h2>
+                {lastInputFoodItems.length > 0 ?
+                    <div style={{display: 'flex', gap: '10px', width: '80%', alignItems: 'center'}}>
+                        {lastInputFoodItems && lastInputFoodItems.length > 0 && lastInputFoodItems.map((food: IFoodItem) =>
+                            <LastFoodAdded
+                                key={food.foodName}
+                                foodName={food.foodName}
+                                fat={food.fat}
+                                protein={food.protein}
+                                carbohydrate={food.carbohydrate}
+                                calories={food.calories}
+                                weight={food.weight}
+                            />
+                        )}
+                    </div> : <h3>Add your first food!</h3>
+                }
+
             </div>
         </div>
     )
