425260ff699a6706d5fc37fb171d5190c8392a22
kurylko <reviliya@gmail.com>
2024-09-26 12:20:58 +0200

    test configurations

--- cypress/e2e/calculator.cy.ts
+++ cypress/e2e/calculator.cy.ts
@@ -1,3 +1,4 @@
+/// <reference types="cypress" />
 export {}
 
 describe('Calculator test', () => {
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,5 @@
 
+export {}
 /// <reference types="cypress" />
 // ***********************************************
 // This example commands.ts shows you how to
