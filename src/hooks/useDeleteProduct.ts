34fbcb45bbf536c2e3e8b99d51da16a2259a1886
kurylko <reviliya@gmail.com>
2024-10-02 14:30:51 +0200

    hook for deleting food item

---
+++ src/hooks/useDeleteProduct.ts
@@ -0,0 +1,39 @@
+import {useState} from 'react';
+import {doc, deleteDoc} from 'firebase/firestore';
+import {db} from '../firebase';
+import {IUserFoodItem} from "../interfaces/FoodItem";
+
+const useDeleteProduct = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<null | Error>(null);
+
+
+    const deleteProduct = async (collectionName: string, data: IUserFoodItem) => {
+        setLoading(true);
+        setError(null);
+
+        if (!data.id) {
+            setError(new Error("Document ID is missing"));
+            setLoading(false);
+            return;
+        }
+
+
+        try {
+            const docRef = doc(db, collectionName, data.id);
+            await deleteDoc(docRef);
+            console.log("Document deleted with ID: ", docRef.id);
+        } catch (err) {
+            console.error("Error deleting document: ", err);
+            setError(err as Error);
+        } finally {
+            setLoading(false);
+        }
+
+        setLoading(false);
+    };
+
+    return {loading, deleteProduct, error};
+}
+
+export default useDeleteProduct;
