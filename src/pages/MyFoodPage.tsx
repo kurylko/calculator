9aead0caf55aab24e4360fce1563d71ac013b422
kurylko <reviliya@gmail.com>
2024-10-02 16:00:17 +0200

    handling data deleting from food list of user

--- src/pages/MyFoodPage.tsx
+++ src/pages/MyFoodPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import useFetchProducts from "../hooks/useFetchProducts";
 import {SavedFoodCard} from "../components/SavedFoodCard";
 import {useAuth} from "../contexts/authContext/authContext";
@@ -8,28 +8,18 @@ import useDeleteProduct from "../hooks/useDeleteProduct";
 
 
 export default function MyFoodPage() {
+    const [usersFoodList, setUsersFoodList] = useState<IUserFoodItem[]>([]);
+
     const { data } = useFetchProducts();
     const {deleteProduct} = useDeleteProduct();
     const {currentUser, loading} = useAuth();
     const uid = currentUser?.uid;
-
-
-    const handleDeleteProduct = async (foodItem: IUserFoodItem): Promise<void> => {
-        if (foodItem.id) {
-            deleteProduct("products", foodItem);
-        } else {
-            console.error("No ID found for this food item");
-        }
-    };
-
-    console.log("current user:", currentUser?.uid);
 
     const getUsersAddedFood = (currentUser: FirebaseUser | null) => {
         let usersAddedFood: IUserFoodItem[] = [];
         if(currentUser !== null){
             usersAddedFood = data;
         } else {
-            console.log(2)
            const localStorageFoodItems = localStorage.getItem('lastInputFoodItems');
            if(localStorageFoodItems) {
                usersAddedFood = JSON.parse(localStorageFoodItems);
@@ -38,16 +28,37 @@ export default function MyFoodPage() {
         return usersAddedFood;
     }
 
-    const foodList = getUsersAddedFood(currentUser);
-    const finalUsersFoodList = foodList.filter(food => food.userID === uid);
 
-    console.log("foodList", foodList);
+    useEffect(() => {
+        const initialFoodList = getUsersAddedFood(currentUser);
+        setUsersFoodList(initialFoodList.filter(food => food.userID === uid));
+    }, [currentUser, uid, data]);
+
+
+    const deleteProductFromLocalStorage = (foodItem: IUserFoodItem) => {
+        const localStorageFoodItems = localStorage.getItem('lastInputFoodItems');
+        const foodItems: IUserFoodItem[] = localStorageFoodItems ? JSON.parse(localStorageFoodItems) : [];
+        const updatedFoodItems = foodItems.filter(item => item.foodName !== foodItem.foodName);
+        localStorage.setItem('lastInputFoodItems', JSON.stringify(updatedFoodItems));
+    }
+
+    const handleDeleteProduct = async (foodItem: IUserFoodItem): Promise<void> => {
+        if (foodItem.id) {
+            await deleteProduct("products", foodItem);
+        } if (foodItem.foodName) {
+            deleteProductFromLocalStorage(foodItem);
+        } else {
+            console.error("No ID found for this food item");
+        }
+    };
+
+    console.log("foodList", usersFoodList);
 
     return (
         <div style={{width: '100%', display: 'flex', flexDirection: 'column', gap: '50px', alignItems: 'center', marginTop: '50px', marginLeft: '50px'}}>
             <h1>MY FOOD </h1>
             <div style={{width: '100%', display: 'flex', flexWrap: "wrap", gap: '50px', alignItems: 'center', marginTop: '10px'}}>
-                {!loading && !!finalUsersFoodList.length && finalUsersFoodList.map(item =>
+                {!loading && !!usersFoodList.length && usersFoodList.map(item =>
                     <SavedFoodCard
                         key={item.foodName}
                         foodName={item.foodName}
