76157c22527d30851d62b6f114d7f7e0067a8f65
kurylko <reviliya@gmail.com>
2024-10-04 21:28:38 +0200

    calculation for single product works

--- src/components/EstimateFoodCalculator.tsx
+++ src/components/EstimateFoodCalculator.tsx
@@ -126,21 +126,126 @@ export const EstimateFoodCalculator: React.FC<EstimateFoodCalculatorProps> = ({
       };
     };
 
-    if (
-      estimateFoodInputsValues.fat === "" &&
-      estimateFoodInputsValues.carbohydrate === "" &&
-      estimateFoodInputsValues.protein === "" &&
-      estimateFoodInputsValues.calories !== ""
-    ) {
-      return calculateForEstimateCalories();
-    }
+    const calculateForEstimateFat = () => {
+      const fatValue = parseFloat(nutriValues.fatValuePerKg);
+      const calculatedWeight = Math.round(
+          (parseFloat(estimateFoodInputsValues.fat as string) /
+              fatValue) *
+          1000,
+      );
+      const calculatedProtein = Math.round(
+          (parseFloat(nutriValues.proteinValuePerKg) / 10000) * calculatedWeight,
+      );
+      const calculatedCarbohydrate = Math.round(
+          (parseFloat(nutriValues.carbohydrateValuePerKg) / 10000) *
+          calculatedWeight,
+      );
+      const calculatedCalories = Math.round(
+          (parseFloat(nutriValues.carbohydrateValuePerKg) / 10000) *
+          calculatedWeight,
+      );
+      return {
+        foodName: selectedProduct,
+        fat: estimateFoodInputsValues.fat,
+        protein: `${calculatedProtein}`,
+        carbohydrate: `${calculatedCarbohydrate}`,
+        calories: `${calculatedCalories}`,
+        weight: `${calculatedWeight}`,
+      };
+    };
+
+    const calculateForEstimateProtein = () => {
+      const proteinValue = parseFloat(nutriValues.proteinValuePerKg);
+      const calculatedWeight = Math.round(
+          (parseFloat(estimateFoodInputsValues.protein as string) /
+              proteinValue) *
+          1000,
+      );
+      const calculatedFat = Math.round(
+          (parseFloat(nutriValues.fatValuePerKg) / 10000) * calculatedWeight,
+      );
+      const calculatedCarbohydrate = Math.round(
+          (parseFloat(nutriValues.carbohydrateValuePerKg) / 10000) *
+          calculatedWeight,
+      );
+      const calculatedCalories = Math.round(
+          (parseFloat(nutriValues.carbohydrateValuePerKg) / 10000) *
+          calculatedWeight,
+      );
+      return {
+        foodName: selectedProduct,
+        fat: `${calculatedFat}`,
+        protein: estimateFoodInputsValues.protein,
+        carbohydrate: `${calculatedCarbohydrate}`,
+        calories: `${calculatedCalories}`,
+        weight: `${calculatedWeight}`,
+      };
+    };
+
+    const calculateForEstimateCarbohydrate = () => {
+      const carbohydrateValue = parseFloat(nutriValues.carbohydrateValuePerKg);
+      const calculatedWeight = Math.round(
+          (parseFloat(estimateFoodInputsValues.carbohydrate as string) /
+              carbohydrateValue) *
+          1000,
+      );
+      const calculatedFat = Math.round(
+          (parseFloat(nutriValues.fatValuePerKg) / 10000) * calculatedWeight,
+      );
+      const calculatedProtein = Math.round(
+          (parseFloat(nutriValues.proteinValuePerKg) / 10000) *
+          calculatedWeight,
+      );
+      const calculatedCalories = Math.round(
+          (parseFloat(nutriValues.caloriesValuePerKg) / 10000) *
+          calculatedWeight,
+      );
+      return {
+        foodName: selectedProduct,
+        fat: `${calculatedFat}`,
+        protein: `${calculatedProtein}`,
+        carbohydrate: estimateFoodInputsValues.carbohydrate,
+        calories: `${calculatedCalories}`,
+        weight: `${calculatedWeight}`,
+      };
+    };
+
+     if (
+        estimateFoodInputsValues.fat !== "" &&
+        estimateFoodInputsValues.carbohydrate === "" &&
+        estimateFoodInputsValues.protein === "" &&
+        estimateFoodInputsValues.calories === ""
+    ){
+      return calculateForEstimateFat();
+    } else if (
+        estimateFoodInputsValues.fat === "" &&
+        estimateFoodInputsValues.carbohydrate !== "" &&
+        estimateFoodInputsValues.protein === "" &&
+        estimateFoodInputsValues.calories === ""
+    ){
+      return calculateForEstimateCarbohydrate();
+    } else if (
+        estimateFoodInputsValues.fat === "" &&
+        estimateFoodInputsValues.carbohydrate === "" &&
+        estimateFoodInputsValues.protein !== "" &&
+        estimateFoodInputsValues.calories === ""
+    ){
+      return calculateForEstimateProtein();
+    } else if (
+         estimateFoodInputsValues.fat === "" &&
+         estimateFoodInputsValues.carbohydrate === "" &&
+         estimateFoodInputsValues.protein === "" &&
+         estimateFoodInputsValues.calories !== ""
+     ) {
+       return calculateForEstimateCalories();
+     }
 
     return {
       foodName: selectedProduct,
-      fat: nutriValues.fatValuePerKg,
-      protein: nutriValues.proteinValuePerKg,
-      carbohydrate: nutriValues.carbohydrateValuePerKg,
-      calories: estimateFoodInputsValues.calories,
+      fat: "N/A",
+      protein: "N/A",
+      carbohydrate: "N/A",
+      calories: "N/A",
       weight: "N/A",
     };
   };
