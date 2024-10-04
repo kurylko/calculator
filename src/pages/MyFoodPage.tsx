867e326e61277d34e0fd19b580c24c6c504e8f0a
kurylko <reviliya@gmail.com>
2024-10-04 21:57:02 +0200

    fixed eslint

--- src/pages/MyFoodPage.tsx
+++ src/pages/MyFoodPage.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import useFetchProducts from "../hooks/useFetchProducts";
 import { SavedFoodCard } from "../components/SavedFoodCard";
 import { useAuth } from "../contexts/authContext/authContext";
