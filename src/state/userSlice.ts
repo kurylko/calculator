708121a26fe234c84b9ebe095c32dc917ce160a2
kurylko <reviliya@gmail.com>
2024-11-13 22:13:27 +0100

    user actions

--- src/state/userSlice.ts
+++ src/state/userSlice.ts
@@ -1,6 +1,10 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { IUser } from '../interfaces/User';
 import { auth } from '../firebase';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
 
 interface UserState {
   currentUser: IUser | null;
@@ -13,6 +17,11 @@ const initialState: UserState = {
   isLoading: false,
   error: null,
 };
+
+interface CreateUserArgs {
+  email: string;
+  password: string;
+}
 
 export const getCurrentUser = createAsyncThunk<
   IUser,
@@ -44,6 +53,66 @@ export const getCurrentUser = createAsyncThunk<
   }
 });
 
+export const createUser = createAsyncThunk<
+  IUser,
+  CreateUserArgs,
+  { rejectValue: string }
+>(
+  'currentUser/createUser',
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      const newUser = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password,
+      );
+      return {
+        uid: newUser.user.uid,
+        email: newUser.user.email || '',
+      };
+    } catch (error) {
+      console.error('Current user error:', error);
+      return rejectWithValue('Failed to create a user');
+    }
+  },
+);
+
+export const logInUser = createAsyncThunk<
+  IUser,
+  CreateUserArgs,
+  { rejectValue: string }
+>('currentUser/logInUser', async ({ email, password }, { rejectWithValue }) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    const currentUser = auth.currentUser;
+    if (currentUser) {
+      const userData: IUser = {
+        uid: currentUser.uid,
+        email: currentUser.email ?? undefined,
+      };
+      return userData;
+    } else {
+      return rejectWithValue('No user is currently logged in');
+    }
+  } catch (error) {
+    console.error('Current user error:', error);
+    return rejectWithValue('Failed to create a user');
+  }
+});
+
+export const logOutUser = createAsyncThunk<null, void, { rejectValue: string }>(
+  'currentUser/logOutUser',
+  async (_, { rejectWithValue }) => {
+    try {
+      await auth.signOut();
+      return null;
+    } catch (error) {
+      console.error('Current user error:', error);
+      return rejectWithValue('Failed to log out a user');
+    }
+  },
+);
+
 const userSlice = createSlice({
   name: 'currentUser',
   initialState,
@@ -62,6 +131,39 @@ const userSlice = createSlice({
       .addCase(getCurrentUser.rejected, (state, action) => {
         state.isLoading = false;
         state.error = action.error.message || 'Failed to fetch current user';
+      })
+      .addCase(createUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.currentUser = action.payload;
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message || 'Failed to create new user';
+      })
+      .addCase(logInUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(logInUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.currentUser = action.payload;
+      })
+      .addCase(logInUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message || 'Failed to log in user';
+      })
+      .addCase(logOutUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(logOutUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.currentUser = action.payload;
+      })
+      .addCase(logOutUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message || 'Failed to log out user';
       });
   },
 });
