b0cd1c3d62a6b76d61e5c7ac46467611d7fa31c2
kurylko <reviliya@gmail.com>
2024-12-14 13:07:02 +0100

    added dialog window to my food page

--- src/components/FoodFormDialog.tsx
+++ src/components/FoodFormDialog.tsx
@@ -8,6 +8,7 @@ import DialogActions from '@mui/material/DialogActions';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import Typography from '@mui/material/Typography';
+import {Box} from "@mui/material";
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
   '& .MuiDialogContent-root': {
@@ -39,7 +40,7 @@ export const FoodFormDialog: React.FC<FoodFormDialogProps> = ({
         open={openDialog}
       >
         <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
-          Modal title
+          Edit food info
         </DialogTitle>
         <IconButton
           aria-label="close"
@@ -54,21 +55,7 @@ export const FoodFormDialog: React.FC<FoodFormDialogProps> = ({
           <CloseIcon />
         </IconButton>
         <DialogContent dividers>
-          <Typography gutterBottom>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-            ac consectetur ac, vestibulum at eros.
-          </Typography>
-          <Typography gutterBottom>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
-            auctor.
-          </Typography>
-          <Typography gutterBottom>
-            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo
-            cursus magna, vel scelerisque nisl consectetur et. Donec sed odio
-            dui. Donec ullamcorper nulla non metus auctor fringilla.
-          </Typography>
+          <Box>Form</Box>
         </DialogContent>
         <DialogActions>
           <Button autoFocus onClick={handleClose}>
